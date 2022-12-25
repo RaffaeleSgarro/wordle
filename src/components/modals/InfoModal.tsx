d66e79a09f5702ccfbe704a683a5c02f2bbdffd5
Raffaele Sgarro <raffaelesgarro@gmail.com>
2022-12-25 11:28:02 +0100

    Traduzioni varie

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,64 +8,52 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Istruzioni" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Indovina la parola segreta! Dopo ogni tentativo il colore di ciascuna lettera diventa un indizio per il tentativo successivo.
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="F"
           status="correct"
         />
-        <Cell value="E" isCompleted={true} />
-        <Cell value="A" isCompleted={true} />
         <Cell value="R" isCompleted={true} />
-        <Cell value="Y" isCompleted={true} />
+        <Cell value="A" isCompleted={true} />
+        <Cell value="S" isCompleted={true} />
+        <Cell value="E" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        La lettera F si trova nel posto giusto!
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
-        <Cell value="P" isCompleted={true} />
+        <Cell value="F" isCompleted={true} />
         <Cell value="I" isCompleted={true} />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="U"
           status="present"
         />
-        <Cell value="O" isCompleted={true} />
-        <Cell value="T" isCompleted={true} />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="mb-1 mt-4 flex justify-center">
-        <Cell value="V" isCompleted={true} />
-        <Cell value="A" isCompleted={true} />
-        <Cell value="G" isCompleted={true} />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="M" isCompleted={true} />
         <Cell value="E" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        La parola contiene la lettera U ma in un'altra posizione
       </p>
 
-      <p className="mt-6 text-sm italic text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="font-bold underline"
-        >
-          check out the code here
-        </a>{' '}
+      <div className="mb-1 mt-4 flex justify-center">
+        <Cell value="A" isCompleted={true} />
+        <Cell value="M" isCompleted={true} />
+        <Cell value="O" isCompleted={true} />
+        <Cell isRevealing={true} isCompleted={true} value="R" status="absent" />
+        <Cell value="E" isCompleted={true} />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        La lettera R non compare nella parola
       </p>
     </BaseModal>
   )
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,22 +1,22 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Ottimo lavoro!', 'Fantastico', 'Ben fatto!']
+export const GAME_COPIED_MESSAGE = 'Copiato negli appunti'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Troppo corto'
+export const WORD_NOT_FOUND_MESSAGE = 'Non consentito'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
 export const HARD_MODE_DESCRIPTION =
   'Any revealed hints must be used in subsequent guesses'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `La parola era ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `La lettera ${guess} si trova nella casella ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
+  `La parola deve contenere ${letter}`
+export const ENTER_TEXT = 'Prova!'
+export const DELETE_TEXT = '\u232B'
 export const STATISTICS_TITLE = 'Statistics'
 export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
 export const NEW_WORD_TEXT = 'New word in'
